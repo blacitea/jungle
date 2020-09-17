0b0fa847de6a4a7c4bb6ac94fee0fecb95d28248
blacitea <angelhoikei@gmail.com>
2020-09-17 16:55:22 +0000

    remove bebug code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,29 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
+    # @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
+
+
+  # def enhanced_order
+  #   # when select, value of other keys not load, but keys still exist
+  #   #@list_of_items = LineItem.where(order_id: @order.id).product
+  #   puts "Order id is: #{@order.id}"
+  #   puts "LineItem with correct Order ID: #{LineItem.where(order_id: @order.id).inspect}"
+  #   puts "We join the tables #{Product.joins(:line_items).inspect}"
+  #   @list_of_items = Product.joins(:line_items).merge(LineItem.where(order_id: @order.id))
+  #   #   @list_of_items = LineItem.where(order_id: @order.id).select("product_id").map { |item| item.product_id}
+  #   puts "list of items "
+  #   p @list_of_items
+  #   # list = @list_of_items.map do |item|
+  #   #   item.product_id
+  #   # end
+  #   # puts list
+  #   # render @list_of_items as :list_of_items pass explicit to review
+  #   # @enhanced_order = Product.where(id: @list_of_items)
+  #   # puts @enhanced_order.inspect
+  #   # @produce_list = @enhanced_order.map {|product| { product:product, quantity: @order[product.id.to_s] } }
+  #   # p @produce_list
+  # end
+  # helper_method :enhanced_order
+
 end
