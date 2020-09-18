9075083bc11629fd1659c4acb1847dc0e54101c7
blacitea <angelhoikei@gmail.com>
2020-09-18 03:49:55 +0000

    add current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,16 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  # def authorize
+  #   redirect_to '/login' unless current_user
+  # end
+
+
     # @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
 
 
