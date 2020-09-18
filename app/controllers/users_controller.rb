d3ac01d7a60e95d4ee717f56f20b64c125d1210e
blacitea <angelhoikei@gmail.com>
2020-09-18 03:51:11 +0000

    correct redirect to => redirect_to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      rediect to '/'
+      redirect_to '/'
     else
-      rediect to '/signup'
+      redirect_to '/signup'
     end
   end
 
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    paras.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
