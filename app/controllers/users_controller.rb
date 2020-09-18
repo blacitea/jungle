10083900cb7386eaf13118b77d6f0bd45f49bdd9
blacitea <angelhoikei@gmail.com>
2020-09-18 00:42:15 +0000

    add create method for user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      rediect to '/'
+    else
+      rediect to '/signup'
   end
+
+
+  private
+
+  def user_params
+    paras.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
 end
