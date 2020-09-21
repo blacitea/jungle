1e1fbeea78814d80af3c8f67b4580a9976f9abfd
blacitea <angelhoikei@gmail.com>
2020-09-21 23:44:33 +0000

    add test case for name nil

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -11,5 +11,19 @@ RSpec.describe Product, type: :model do
         expect(@product.save).to be true
       end
     end
+
+    context 'given nil for 1 of the required inputs' do
+      context 'given nil for name' do
+        it 'should return error with name required' do
+          @category = Category.create(name: 'Toys')
+          @product = @category.products.build(name: nil, price: 100000, quantity: 1)
+          expect(@product.valid?).to be false
+          expect(@product.errors.any?).to be true
+          expect(@product.errors[:name].any?).to be true
+          expect(@product.errors.full_messages).to include('Name can\'t be blank')
+        end
+      end
+    end
+
   end
 end
