192f8c0f37c2ed2596960e39bae1a8b852d9000d
blacitea <angelhoikei@gmail.com>
2020-09-21 23:55:53 +0000

    move @category initialize to a before block

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,10 +3,12 @@ require_relative '../../app/models/Product'
 
 RSpec.describe Product, type: :model do
   describe 'Validation' do
+    before do
+      @category = Category.create(name: 'Toys')
+    end
+
     context 'given valid input for all fields' do
       it 'should save successfully' do
-        @category = Category.create(name: 'Toys')
-        #p @category
         @product = @category.products.build(name: 'Pikachu', price: 100000, quantity: 1)
         expect(@product.save).to be true
       end
@@ -15,7 +17,6 @@ RSpec.describe Product, type: :model do
     context 'given nil for 1 of the required inputs' do
       context 'given nil for name' do
         it 'should return error with name required' do
-          @category = Category.create(name: 'Toys')
           @product = @category.products.build(name: nil, price: 100000, quantity: 1)
           expect(@product.valid?).to be false
           expect(@product.errors.any?).to be true
@@ -26,7 +27,6 @@ RSpec.describe Product, type: :model do
 
       context 'given nil for price' do
         it 'should return error with price required' do
-          @category = Category.create(name: 'Toys')
           @product = @category.products.build(name: 'Piko', price: nil, quantity: 1)
           expect(@product.valid?).to be false
           expect(@product.errors.any?).to be true
@@ -37,7 +37,6 @@ RSpec.describe Product, type: :model do
 
       context 'given nil for quantity' do
         it 'should return error with quantity required' do
-          @category = Category.create(name: 'Toys')
           @product = @category.products.build(name: 'Piko', price: 5000, quantity: nil)
           expect(@product.save).to be false
           expect(@product.errors.full_messages.size).to eq(1)
@@ -47,7 +46,6 @@ RSpec.describe Product, type: :model do
 
       context 'given nil for category' do
         it 'should return error with category required' do
-          @category = Category.create(name: 'Toys')
           @product = Product.create(name: 'Piko', price: 5000, quantity: 10)
           expect(@product.save).to be false
           expect(@product.errors.full_messages.size).to eq(1)
