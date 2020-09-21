7c2b607c123ad46984daf0fb29640ba1729dbb9f
blacitea <angelhoikei@gmail.com>
2020-09-21 23:24:58 +0000

    create happy test case

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
+require_relative '../../app/models/Product'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+    context 'given valid input for all fields' do
+      it 'should save successfully' do
+        @category = Category.create(name: 'Toys')
+        #p @category
+        @product = @category.products.build(name: 'Pikachu', price: 100000, quantity: 1)
+        expect(@product.save).to be true
+      end
+    end
+  end
 end
