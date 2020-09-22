c49a49d1c7ebd32f37d66374260328ac7a5ae322
blacitea <angelhoikei@gmail.com>
2020-09-22 02:22:10 +0000

    add test case and validation for first_name presence

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
+require_relative '../../app/models/User'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+    context 'missing 1 required field' do
+      it 'cannot create when missing first name' do
+        @user = User.create(first_name:nil)
+        expect(@user.errors.any?).to be true
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+    end
+
+    context 'given valid inputs' do
+      it 'creates a user instance successfully' do
+        @user = User.new(password: 'medley', password_confirmation: 'medley')
+        p @user
+        expect(@user.save).to be false
+      end
+    end
+  end
 end
