dd232107e511e5af6d05956e9cacc5a5f2cde9fa
blacitea <angelhoikei@gmail.com>
2020-09-22 21:58:53 +0000

    add spec for user_login, use fill_in and click_button for form manipulation

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "User authentication", type: :feature, js: true do
+  before :each do
+    user = User.create!(first_name: 'Alice', last_name: 'House', email: 'ah@ym.com', password: 'light', password_confirmation: 'light')
+  end
+
+  scenario "can login with correct credentials" do
+    visit root_path
+    page.find_link('Login').click
+    expect(page).to have_content(/password/i)
+    fill_in 'Email', with: 'ah@ym.com'
+    fill_in 'Password', with: 'light'
+    #puts page.html
+    page.click_button 'Login' #click_button must append a page
+    #save_screenshot
+    expect(page).to have_content('Signed in as Alice')
+  end
+end
