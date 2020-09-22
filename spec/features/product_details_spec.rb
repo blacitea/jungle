759bd91572e27239af4c5d6bf31806a3575cb73c
blacitea <angelhoikei@gmail.com>
2020-09-22 21:01:45 +0000

    use find_link(Details) to search for clickable link

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details page", type: :feature, js: true do
+
+  # SETUP
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see details of the product" do
+    # ACT
+    visit root_path
+    expect(page).to have_css 'article.product' ,count: 10
+    product = page.find('article.product', match: :first)
+    save_screenshot
+    # use Regexp to find button
+    product.find_link('Details').click
+    expect(page).to have_content 'Description'
+
+    # DEBUG / VERIFY
+  end
+
+end
